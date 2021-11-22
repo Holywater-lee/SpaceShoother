2c676de1ef7008d003eb245773ff96f4364ad18e
20181220 이성수 <gentlemaker@naver.com>
2021-11-22 14:49:02 +0900

    씬 로드 기능 추가

--- SpaceShooter/Assets/02. Scripts/UIMgr.cs
+++ SpaceShooter/Assets/02. Scripts/UIMgr.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIMgr : MonoBehaviour
 {
 	public void OnClickStartBtn()
 	{
 		Debug.Log("Click Button");
+		SceneManager.LoadScene("SCPlay");
 	}
 }
